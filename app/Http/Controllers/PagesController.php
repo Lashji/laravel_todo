fb41470028a95c367e93e7e4587235b8f622730d
Lassi Palojarvi <lassi.palojarvi@outlook.com>
2017-12-08 22:47:59 +0200

    v1

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function new()
+    {
+        // echo "some echoed data";
+
+        return view('new');
+    }
+}
